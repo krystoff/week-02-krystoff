1b258349c83bb3ae531b07a68a0345676e3b0ae3
Andy Litzinger <andy.litzinger@gmail.com>
2011-10-16 21:30:02 -0700

    fixed the broke tests

--- spec/exercise_spec.rb
+++ spec/exercise_spec.rb
@@ -1,21 +1,16 @@
 describe "String Test" do
   context "when calling strip" do
-
     it "should remove all whitespace from the beginning and the end of the string" do
-
         lyrics = "  Hello, is it me you're looking for I can see it in your eyes  "
-
-        lyrics.strip.should == "Hello, is it me you're looking for I can see it in your eyes "
+        lyrics.strip.should == "Hello, is it me you're looking for I can see it in your eyes"
     end
-
   end
-
 end
 
 describe "Integer Test" do
   context "when measuring length of a String" do
     it "should return a specific integer" do
-      sum_string = (1 + 3 + 8)
+      sum_string = '1 + 3 + 8'
       sum_string.length.should eq 9
     end
   end
